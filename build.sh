4c6b79dae1c66d81016776a4b57d3ec038faf300
Illusion137 <raygoza.d@hotmail.com>
2026-03-08 03:23:08 -0700

    chore: set -e again, but force remove so there is no errors;

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 EVERETT_PATH="/Users/illusion/dev/Everett"
 
 ts-node unit_gen/unit_gen.ts
@@ -8,9 +9,9 @@ export CMAKE_BUILD_PARALLEL_LEVEL=8
 # emcmake cmake -B build-wasm -S .
 emcmake cmake -S . -B build-wasm
 cmake --build build-wasm
-rm $EVERETT_PATH/public/wasm/Nero.js
-rm $EVERETT_PATH/public/wasm/Nero.wasm
-rm $EVERETT_PATH/src/dimension_wasm_interface.ts
+rm -f $EVERETT_PATH/public/wasm/Nero.js
+rm -f $EVERETT_PATH/public/wasm/Nero.wasm
+rm -f $EVERETT_PATH/src/dimension_wasm_interface.ts
 rm -f $EVERETT_PATH/src/Nero.d.ts
 cp build-wasm/Nero.js $EVERETT_PATH/public/wasm/Nero.js
 cp build-wasm/Nero.wasm $EVERETT_PATH/public/wasm/Nero.wasm
